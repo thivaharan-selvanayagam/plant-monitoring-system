94d7154c3d82805ed8756cf215b75326e9baab0c
thivaharan-selvanayagam <101883467+thivaharan-selvanayagam@users.noreply.github.com>
2022-03-18 20:57:50 +0530

    initial commit

---
+++ garden_blynk_motor.ino
@@ -0,0 +1,47 @@
+int soilMoistureValue1 = 0;
+int IN1 = 34;
+int Pin1 = 13;
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+char auth[] = "hN3ZdXhURen0vSlO8odthjEoB3na3IfU";
+
+BlynkTimer timer;
+char ssid[] = "ZTE";
+char pass[] = "Thiva@!1995";
+
+void myTimerEvent()
+{
+  Blynk.virtualWrite(V34, analogRead(34));
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+
+  timer.setInterval(1000L, myTimerEvent);
+  pinMode(Pin1, OUTPUT);
+  
+  pinMode(IN1, INPUT);
+  
+  digitalWrite(Pin1, HIGH);
+}
+
+void loop()
+{
+   soilMoistureValue1 = analogRead(IN1);
+   Serial.println(soilMoistureValue1);
+
+   if (soilMoistureValue1 > 3500) {
+       digitalWrite(Pin1, LOW);
+   } else {
+       digitalWrite(Pin1, HIGH);
+   }
+  delay(1000);
+  Blynk.run();
+  timer.run();
+}
